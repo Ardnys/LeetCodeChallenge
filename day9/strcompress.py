ea9d4b4dc90259e08852fd83b3c7066d7bfbb050
Ardnys <semresavas35@gmail.com>
2023-11-11 22:48:53 +0300

    solve str compress

--- day9/strcompress.py
+++ day9/strcompress.py
@@ -1,18 +1,26 @@
 def compress(chars) -> int:
-    for char in chars:
-        if char in map:
-            map[char] += 1
+    l = []
+    count = 1
+    for i in range(1, len(chars)):
+        if chars[i] == chars[i-1]:
+            count += 1
         else:
-            map[char] = 1
+            l.append((chars[i-1], count))
+            count = 1
+    l.append((chars[-1], count))
 
-    compressed_str = ""
-    for key, val in map.items():
-        compressed_str += str(key)
-        compressed_str += str(val)
+    uh = 0
+    for key, val in l:
+        chars[uh] = key
+        uh += 1
+        if val > 1:
+            for digit in str(val):
+                chars[uh] = str(digit)
+                uh += 1
 
-
-    return len(compressed_str)
+    return uh
 
 
 chars = ["a", "a", "b", "b", "c", "c", "c"]
 l = compress(chars)
+print(l)
