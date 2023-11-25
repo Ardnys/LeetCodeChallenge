4942b96662ab0dbfe7e2f88e085c6c4c13e216ec
Ardnys <semresavas35@gmail.com>
2023-11-25 20:34:44 +0300

    llist comprehension]

--- day25/isarchnemesis.py
+++ day25/isarchnemesis.py
@@ -1,36 +1,31 @@
 def isPalindrome(s: str) -> bool:
-    i = 0
-    j = len(s) - 1
-    while i < j:
-        if not s[i].isalnum():
-            i += 1
-            continue
-        elif not s[j].isalnum():
-            j -= 1
-            continue
-        elif s[i].lower() != s[j].lower():
-            return False
-        i += 1
-        j -= 1
-
-    return True
-
-    # a = []
-    # for c in s:
-    #     if c.isalnum():
-    #         a.append(c.lower())
-    #
     # i = 0
-    # j = len(a) - 1
-    #
+    # j = len(s) - 1
     # while i < j:
-    #     if a[i] != a[j]:
+    #     if not s[i].isalnum():
+    #         i += 1
+    #         continue
+    #     elif not s[j].isalnum():
+    #         j -= 1
+    #         continue
+    #     elif s[i].lower() != s[j].lower():
     #         return False
     #     i += 1
     #     j -= 1
     #
     # return True
     #
+    s = [c.lower() for c in s if c.isalnum()]
+    i = 0
+    j = len(s) - 1
+
+    while i < j:
+        if s[i] != s[j]:
+            return False
+        i += 1
+        j -= 1
+
+    return True
 
 
 s = "A man, a plan, a canal: Panama"
